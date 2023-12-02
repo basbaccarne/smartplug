7ccb058fbe0dbaef9b705c2d99fa6a5897daa59e
Bas Baccarne <bastiaan.baccarne@ugent.be>
2023-12-02 10:45:33 +0100

    test 2 valid

--- tests/2_WiFi _Webhook.ino
+++ tests/2_WiFi _Webhook.ino
@@ -1,20 +1,23 @@
 // test 2: connect XIAO esp32s3 to WiFi & send webhook
 
-// load libraries for WiFi & Webhook 
+// Load WiFi libraries
 #include "WiFi.h"
-#include <IFTTTWebhook.h>
+#include "HTTPClient.h"
 
-// set Wifi credentials
-// passwords are stored in a seperate "arduino_secrets.h" file
+// Set passwords for WiFi
 #include "arduino_secrets.h"
 char ssid[] = SECRET_SSID;
 char password[] = SECRET_PASS;
+char hostname[] = "ESP23S_lightsensor";
 
-// set IFTTT webhook credentials
-char api_key[] = IFTTT_API_KEY;
-char ifttt_event[] = IFTTT_EVENT_NAME;
+// Set IFTTT credentials (get event & API key from "arduino_secrets.h")
+const String event = IFTTT_EVENT_NAME;
+const String key = IFTTT_API_KEY;
+const String HOST_NAME ="https://maker.ifttt.com";
+const String PATH_NAME= "/trigger/" + event + "/json/with/key/" + key;
+const String queryString = "variable1=1";
 
-// create a WiFi connection function
+// create a function to set-up the WiFi connection
 void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -23,6 +26,7 @@ void initWiFi() {
     Serial.print('.');
     delay(1000);
   }
+  WiFi.hostname(hostname);
   Serial.println();
   Serial.println(WiFi.localIP());
 }
@@ -30,7 +34,7 @@ void initWiFi() {
 void setup() {
   Serial.begin(9600);
 
-   // set WiFi to station mode
+ // set WiFi to station mode
   WiFi.mode(WIFI_STA);
 
   // disconnect from an AP if it was previously connected
@@ -40,13 +44,34 @@ void setup() {
   // attempt to connect to Wifi network:
   initWiFi();
 
-  // define IFTTT webhook
-  Serial.println("Connecting to IFTTT");
-  IFTTTWeb webhook(api_key, ifttt_event);
-  Serial.println("Webhook initialized");
 }
 
 void loop() {
-  webhook.trigger("flush");
+  // define the http client
+  HTTPClient http;
+
+ // open http connection
+  http.begin(HOST_NAME + PATH_NAME);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // post the http payload
+  int httpCode = http.POST(queryString);
+
+  // error handling (httpCode is negative on error)
+  if (httpCode > 0) {
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+
   delay(10000);
 }
